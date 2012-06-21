1ea68b085d61db6a8646fed9bf5ec66faceda22d
Aiti Student <aiti-student@aiti-091-PC.(none)>
2012-06-21 13:10:10 -0400

    answers

---
+++ Lab03_2a.py
@@ -0,0 +1,27 @@
+#archibold_acheampong
+
+print'Lab03_2a'
+n=1
+i=0
+display=""
+line_track=0
+while n<7:
+    while i<n:
+        display+=str(n)
+        i+=1
+        print display
+    n+=1
+'''for i in range(1,7,1):
+    print i,'''
+
+
+print "\n"
+print'Lab03_2b'
+b=7
+i=0
+while i<6:
+    for x in range(1,b):
+        print x,
+    b=b-1
+    print
+    i+=1
